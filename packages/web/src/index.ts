b8fd6fae19e427ee49f882121fd5fb601409a5ea
truman <tfenley23@gmail.com>
2026-08-22 02:00:53 -0400

    feat(web): complete the village and add a one-command dev script

---
+++ packages/web/src/index.ts
@@ -0,0 +1,11 @@
+export const WEB_VERSION = '0.1.0';
+
+export * from './theme.js';
+export * from './render/roles.js';
+export * from './render/compose.js';
+export * from './render/bake.js';
+export * from './render/label.js';
+export * from './motion/motion.js';
+export * from './motion/behaviour.js';
+export * from './layout/zones.js';
+export * from './net/protocol.js';
